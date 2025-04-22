ef4b8c898d60fb4cd2bf0421e9d01f3f9e6567f1
Owen <owen.griere@gmail.com>
2025-04-22 14:46:54 +0200

    debug

--- draw_tysserand_IMC_IF.sh
+++ draw_tysserand_IMC_IF.sh
@@ -24,7 +24,7 @@ python SCRIPT/test_parser.py --file CONFIG/tysserand.yaml
 printf "TEST --- DONE\n\n"
 
 
-mkdir -p ../Tysserand_network
+mkdir -p Tysserand_network
 echo '########### Tysserand Plotting ###########'
 python SCRIPT/draw_tysserand.py --file CONFIG/tysserand.yaml
 
