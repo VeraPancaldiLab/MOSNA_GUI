624e5289f7723a85daafc53830d3b76c72d1e0e8
Owen.GRIERE <owen.griere@gmail.com>
2026-06-08 11:20:34 +0200

    change setup

--- setup.sh
+++ setup.sh
@@ -148,10 +148,10 @@ step "Step 2/4 — mosna-package"
 cd "${MOSNA_PACKAGE_DIR}"
 if ${DEV_MODE}; then
     info "Installing mosna-package in editable/dev mode ..."
-    pip install -e .
+    python -m pip install -e .
 else
     info "Installing mosna-package ..."
-    pip install .
+    python -m pip install .
 fi
 cd "${SCRIPT_DIR}"
 success "mosna-package installed."
